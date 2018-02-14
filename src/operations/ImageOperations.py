e6dea2c03d4202ab31df14d2ce5aea275e4e9dd7
ehab.samir <sw.eng.ehab@gmail.com>
2018-02-14 17:07:11 +0200

    Adding Window for Change Detection Process to set all input and output files Viewing Statistical Report in new Window in addition to saving this report in Xls format

--- src/operations/ImageOperations.py
+++ src/operations/ImageOperations.py
@@ -11,7 +11,6 @@ from PIL import Image
 from PyQt5.QtWidgets import (QFileDialog)
 from src.gui.DGISTMainWindow import DGISTMainWindow
 
-
 class ImageOperations(DGISTMainWindow):
 
     def __init__(self):
@@ -52,7 +51,6 @@ class ImageOperations(DGISTMainWindow):
             plt.show()
             DGISTMainWindow.metadataAct.setEnabled(False)
             DGISTMainWindow.histogramAct.setEnabled(False)
-
 
     def submit(self):
         self.bandRed=1
@@ -197,30 +195,44 @@ class ImageOperations(DGISTMainWindow):
         plt.legend(color, loc='best')
         plt.show()
 
-    def write_geotiff(fname, data, geo_transform, projection, data_type=gdal.GDT_Byte):
+    def write_geotiff(self, fname, data, geo_transform, projection, data_type=gdal.GDT_Byte):
         driver = gdal.GetDriverByName('GTiff')
         rows, cols = data.shape
-        dataset = driver.Create(fname, cols, rows, 1, data_type)
+        dataset = driver.Create(fname, self.cols, self.rows, 1, data_type)
         dataset.SetGeoTransform(geo_transform)
-        dataset.SetProjection(projection)
+        dataset.SetProjectionw(projection)
         band = dataset.GetRasterBand(1)
         band.WriteArray(data)
         dataset = None
         return
 
-    def changeDetection(self):
-        image1, _ = QFileDialog.getOpenFileName(self, "Choose The First Image", QDir.currentPath())
-        image2, _ = QFileDialog.getOpenFileName(self, "Choose The Second Image", QDir.currentPath())
-        outputImageName = QFileDialog.getSaveFileName(self, "Save Change Detection Result", "",
-                                                      "TIF Image (*.tif);;PNG Image (*.png);;BMP Image (*.bmp);;JPG Image (*.jpg);;Text Files (*.txt)")[
-            0]
-        outputMatrixName = QFileDialog.getSaveFileName(self, "Save Change Detection Matrix", "",
-                                                       "Excel XLSX (*.xlsx);;Excel XLS (*.xls);;Text Files (*.txt)")[0]
+    def select_initial_path(self):
+        print "selecting initial path"
+        self.image1, _ = QFileDialog.getOpenFileName(self, "Choose The Input Initial Image", QDir.currentPath())
+        if self.image1:
+            self.entryInitial.delete(0, END)
+            self.entryInitial.insert(0, self.image1)
 
-        raster_before = gdal.Open(image1, gdal.GA_ReadOnly)
-        raster_after = gdal.Open(image2, gdal.GA_ReadOnly)
+    def select_final_path(self):
+        print "selecting final path"
+        self.image2, _ = QFileDialog.getOpenFileName(self, "Choose The Input Final Image", QDir.currentPath())
+        if self.image2:
+            self.entryFinal.delete(0, END)
+            self.entryFinal.insert(0, self.image2)
+
+    def select_img_diff_path(self):
+        print "selecting image difference path"
+        self.image3 = QFileDialog.getSaveFileName(self, "Choose The Output Image Difference", "", "TIF Image (*.tif);;PNG Image (*.png);;BMP Image (*.bmp);;JPG Image (*.jpg);;Text Files (*.txt)")[0]
+        if self.image3:
+            self.entryImgDiff.delete(0, END)
+            self.entryImgDiff.insert(0, self.image3)
+
+    def run_change_detection(self):
+
+        raster_before = gdal.Open(self.image1, gdal.GA_ReadOnly)
+        raster_after = gdal.Open(self.image2, gdal.GA_ReadOnly)
         geo_transform = raster_after.GetGeoTransform()
-        proj = raster_after.GetProjectionRef()
+        #proj = raster_after.GetProjectionRef()
 
         band = raster_before.GetRasterBand(1)
         bands_before = band.ReadAsArray()
@@ -231,45 +243,112 @@ class ImageOperations(DGISTMainWindow):
 
         flat_data = bands_after.reshape((rows_after, cols_after))
 
-        change_mat = np.zeros((7, 7), dtype=np.int)
+        self.change_mat = np.zeros((7, 7), dtype=np.int)
 
         for i in range(rows_after):
             for j in range(cols_after):
-                change_mat[bands_before[i][j]][bands_after[i][j]] += 1
+                self.change_mat[bands_before[i][j]][bands_after[i][j]] += 1
                 flat_data[i][j] = 7 * (bands_before[i][j]) + (bands_after[i][j]) + 1
-            # print str(int(100.0 * i / rows_after))+"%"
-            self.statusBar().showMessage("Difference Image Creation: " + str(int(100.0 * i / rows_after)) + "%")
+            print str(int(100.0 * i / rows_after))+"%"
 
-        self.statusBar().showMessage("Difference Image Created @ ")
+        ## write difference image into the output path
+        driver = gdal.GetDriverByName('GTiff')
+        rows, cols = flat_data.shape
+        dataset = driver.Create(self.image3, cols, rows, 1, gdal.GDT_Byte)
+        dataset.SetGeoTransform(geo_transform)
+        # dataset.SetProjectionw(proj)
+        band = dataset.GetRasterBand(1)
+        band.WriteArray(flat_data)
+        dataset = None
 
-        self.write_geotiff(outputImageName, flat_data, geo_transform, proj)
+        ## view difference image
+        plt.figure(120)
+        plt.gcf().canvas.set_window_title(self.image3)
+        plt.imshow(flat_data)
+        plt.axis('off')
+        plt.show()
 
-        self.statusBar().showMessage("Difference Image Saved @ " + outputImageName)
+        self.btnViewReport.configure(state=NORMAL)
 
-        workbook = xlsxwriter.Workbook(outputMatrixName)
-        ws_stat = workbook.add_worksheet()
+    def save_statistical_report(self):
 
-        classes = ["Class 0", "Class 1", "Class 2", "Class 3", "Class 4", "Class 5", "Class 6"]
+        self.report_path = QFileDialog.getSaveFileName(self, "Save Change Detection Report", "", "Excel XLSX (*.xlsx);;Excel XLS (*.xls);;Text Files (*.txt)")[0]
+        if self.report_path:
+            workbook = xlsxwriter.Workbook(self.report_path)
+            ws_stat = workbook.add_worksheet()
 
-        ws_stat.write(0, 0, "State No.")
-        ws_stat.write(0, 1, "BEFORE")
-        ws_stat.write(0, 2, "AFTER")
-        ws_stat.write(0, 3, "# of pixels")
-        ws_stat.write(0, 4, "Percent (%)")
+            for i in range(0, 7):
+                ws_stat.write_string(0, i + 1, self.classes[i])
+                ws_stat.write_string(i + 1, 0, self.classes[i])
+
+            for col, data in enumerate(self.change_mat):
+                ws_stat.write_column(1, col + 1, data)
+
+    def view_change_detection_report(self):
+
+        self.rootCDR = Tk()
+        self.rootCDR.title("Statistical Report")
+        self.frameCDR = Frame(self.rootCDR)
+        self.frameCDR.pack()
+
+        header_names = ["State No.", "BEFORE", "AFTER", "# of pixels", "Percent (%)"]
+        self.classes = ["Class 0", "Class 1", "Class 2", "Class 3", "Class 4", "Class 5", "Class 6"]
+
+        btnSaveStatReport = Button(self.frameCDR, text="Save Statistical Report", fg="blue", command=self.save_statistical_report, bg="white", font=("SansSerif", 10, "bold"))
+        btnSaveStatReport.grid(row=0, column=0, sticky=E, pady=10, padx=5)
+
+        Label(self.frameCDR, text="          ", font=("SansSerif", 10, "bold")).grid(row=9, column=2, sticky=E, pady=0, padx=0)
+        Label(self.frameCDR, text="          ", font=("SansSerif", 10, "bold")).grid(row=2, column=9, sticky=E, pady=0,padx=0)
+
         for i in range(0, 7):
+            Label(self.frameCDR, text=self.classes[i], width=12, font=("SansSerif", 10, "bold")).grid(row=0, column=i+1, sticky=E, pady=0, padx=0)
+
+        for i in range(0, 7):
+            Label(self.frameCDR, text=self.classes[i], width=12, font=("SansSerif", 10, "bold")).grid(row=i+1, column=0,sticky=E, pady=0, padx=0)
             for j in range(0, 7):
-                ws_stat.write(7 * i + j + 1, 0, 7 * i + j + 1)
-                ws_stat.write(7 * i + j + 1, 1, classes[i])
-                ws_stat.write(7 * i + j + 1, 2, classes[j])
-                ws_stat.write(7 * i + j + 1, 3, change_mat[i][j])
-                ws_stat.write(7 * i + j + 1, 4, 1.0 * change_mat[i][j] / np.sum(change_mat))
+                Button(self.frameCDR, text=str(self.change_mat[i][j]), bg="white", width=12, font=("SansSerif", 10, "bold")).grid(row=i+1, column=j+1, sticky=E, pady=0, padx=0)
 
-        workbook.close()
+        self.rootCDR.mainloop()
 
-        self.statusBar().showMessage("Difference Image Report Saved @ " + outputMatrixName)
-        self.statusBar().showMessage("Change Detection Process finished successfully.")
-        self.statusBar().showMessage("")
+    def changeDetection(self):
 
+        self.rootCD = Tk()
+        self.rootCD.title("Change Detection Process")
+        self.frameCD = Frame(self.rootCD)
+        self.frameCD.pack()
+
+        labelHeader = Label(self.frameCD, text="Change Detection Process", font=("SansSerif", 20, "bold"))
+        labelInitial = Label(self.frameCD, text="Input Initial Image", font=("SansSerif", 12))
+        labelFinal = Label(self.frameCD, text="Input Final Image", font=("SansSerif", 12))
+        labelImgDiff = Label(self.frameCD, text="Output Difference Image", font=("SansSerif", 12))
+
+        self.entryInitial = Entry(self.frameCD, width=70, font=("SansSerif", 12))
+        self.entryFinal = Entry(self.frameCD, width=70, font=("SansSerif", 12))
+        self.entryImgDiff = Entry(self.frameCD, width=70, font=("SansSerif", 12))
+
+        btnChooseInitial = Button(self.frameCD, text="Choose", font=("SansSerif", 12), command=self.select_initial_path)
+        btnChooseFinal = Button(self.frameCD, text="Choose", font=("SansSerif", 12), command=self.select_final_path)
+        btnChooseImgDiff = Button(self.frameCD, text="Choose", font=("SansSerif", 12), command=self.select_img_diff_path)
+        btnRun = Button(self.frameCD, text="Run", font=("SansSerif", 14), command=self.run_change_detection)
+        self.btnViewReport = Button(self.frameCD, text="View Report", font=("SansSerif", 14), command=self.view_change_detection_report)
+        self.btnViewReport.configure(state=DISABLED)
+
+        labelHeader.grid(row=0, column=1, sticky=N, pady=25, padx=15)
+        labelInitial.grid(row=1, column=0, sticky=W, pady=12, padx=5)
+        labelFinal.grid(row=2, column=0, sticky=W, pady=12, padx=5)
+        labelImgDiff.grid(row=3, column=0, sticky=W, pady=12, padx=5)
+
+        self.entryInitial.grid(row=1, column=1, sticky=W, pady=12, padx=5)
+        self.entryFinal.grid(row=2, column=1, sticky=W, pady=12, padx=5)
+        self.entryImgDiff.grid(row=3, column=1, sticky=W, pady=12, padx=5)
+
+        btnChooseInitial.grid(row=1, column=2, sticky=W, pady=12, padx=5)
+        btnChooseFinal.grid(row=2, column=2, sticky=W, pady=12, padx=5)
+        btnChooseImgDiff.grid(row=3, column=2, sticky=W, pady=12, padx=5)
+        btnRun.grid(row=5, column=1, sticky=S, pady=12)
+        self.btnViewReport.grid(row=5, column=1, sticky=E, pady=12)
+
+        self.rootCD.mainloop()
 
 sys._excepthook = sys.excepthook
 
